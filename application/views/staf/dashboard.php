c24c80a874d0df9b06ef524fa76bec88f0c2ee00
kurniawanhandito <kurniawanhandito@gmail.com>
2017-11-17 09:51:24 +0700

    add folder and staf's dashboard page

---
+++ application/views/staf/dashboard.php
@@ -0,0 +1,17 @@
+	<div class="content-wrapper">
+		<div class="container-fluid">
+			<!-- Breadcrumbs-->
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.html">Dashboard</a>
+				</li>
+			</ol>
+			<div class="row">
+				<div class="col-12">
+					<h1>Staf</h1>
+					<p>Selamat Datang Staf</p>
+				</div>
+			</div>
+		</div>
+		<!-- /.container-fluid-->
+		<!-- /.content-wrapper-->
