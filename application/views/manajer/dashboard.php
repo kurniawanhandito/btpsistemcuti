51f88d53d8942f5c77c9dd9e7a9b7c065114199c
kurniawanhandito <kurniawanhandito@gmail.com>
2017-11-17 09:51:09 +0700

    add folder and manajer's dashboard page

---
+++ application/views/manajer/dashboard.php
@@ -0,0 +1,17 @@
+	<div class="content-wrapper">
+		<div class="container-fluid">
+			<!-- Breadcrumbs-->
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.html">Dashboard</a>
+				</li>
+			</ol>
+			<div class="row">
+				<div class="col-12">
+					<h1>Manajer</h1>
+					<p>Selamat Datang Manajer</p>
+				</div>
+			</div>
+		</div>
+		<!-- /.container-fluid-->
+		<!-- /.content-wrapper-->
