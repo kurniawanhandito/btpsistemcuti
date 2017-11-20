d9200375c71be9305d3845462cd28405b31a26e6
kurniawanhandito <kurniawanhandito@gmail.com>
2017-11-20 07:47:45 +0700

    fix icon for card

--- application/views/hrd/dashboard.php
+++ application/views/hrd/dashboard.php
@@ -1,17 +1,41 @@
 	<div class="content-wrapper">
 		<div class="container-fluid">
-			<!-- Breadcrumbs-->
-			<ol class="breadcrumb">
-				<li class="breadcrumb-item">
-					<a href="index.html">Dashboard</a>
-				</li>
-			</ol>
-			<div class="row">
-				<div class="col-12">
-					<h1>HRD</h1>
-					<p>Selamat Datang HRD</p>
-				</div>
-			</div>
+			<h1>Selamat Datang, HRD !</h1>
+		    <hr class="my-4">
+		    <div class="row">
+		    	<div class="col-xl-3 col-sm-6 mb-3">
+          			<div class="card text-white bg-primary o-hidden h-100">
+            			<div class="card-body">
+              				<div class="card-body-icon">
+                				<i class="fa fa-fw fa-users"></i>
+              				</div>
+              				<div class="mr-5">26 Data Karyawan!</div>
+            			</div>
+            			<a class="card-footer text-white clearfix small z-1" href="#">
+              				<span class="float-left">View Details</span>
+              				<span class="float-right">
+                				<i class="fa fa-angle-right"></i>
+              				</span>
+            			</a>
+          			</div>
+        		</div>
+		    	<div class="col-xl-3 col-sm-6 mb-3">
+	          		<div class="card text-white bg-success o-hidden h-100">
+	            		<div class="card-body">
+	              			<div class="card-body-icon">
+	                			<i class="fa fa-fw fa-plane"></i>
+	              			</div>
+	              			<div class="mr-5">123 Data Cuti Karyawan!</div>
+	            		</div>
+	            		<a class="card-footer text-white clearfix small z-1" href="#">
+	              			<span class="float-left">View Details</span>
+	              			<span class="float-right">
+	                			<i class="fa fa-angle-right"></i>
+	              			</span>
+	            		</a>
+	          		</div>
+	        	</div>
+		    </div>
 		</div>
 		<!-- /.container-fluid-->
 		<!-- /.content-wrapper-->
